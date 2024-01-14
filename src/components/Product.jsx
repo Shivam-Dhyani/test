fdb28437763ad5516fce1edea954beff083a8c2b
Shivam-Dhyani <shivamdhiyani15@gmail.com>
2024-01-14 20:01:07 +0530

    C2-1 Functioanlity of Total Price & Quantity Added

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,24 +1,25 @@
-import React, { useState } from "react";
-import {
-  setProductList,
-  setTotalPrice,
-  setTotalQuantity,
-} from "../redux/reducers/products";
-import { useDispatch, useSelector } from "react-redux";
+import { useState } from "react";
+import { setProductList } from "../redux/reducers/products";
+import { useDispatch } from "react-redux";
+import { round } from "lodash";
 
 const Product = ({ product }) => {
   const dispatch = useDispatch();
-  const { totalPrice, totalQuantity } = useSelector(
-    (state) => state.products.products
-  );
   const [quantity, setQuantity] = useState(0);
+
   const handleAddToCart = (product) => {
-    setQuantity((currQuantity) => currQuantity + 1);
-    const currPrice = Number(product.price) * Number(quantity);
-    // console.log("123==>", totalPrice, totalQuantity);
-    // dispatch(setTotalPrice(totalPrice + currPrice));
-    // dispatch(setTotalQuantity(totalQuantity + quantity));
-    dispatch(setProductList({ ...product, quantity }));
+    // Use the functional form of setQuantity to ensure correct previous state
+    setQuantity((prevQuantity) => {
+      // Check if quantity is 0, then increment it
+      const updatedQuantity =
+        prevQuantity === 0 ? prevQuantity + 1 : prevQuantity;
+
+      // Dispatch the action with the updated quantity
+      dispatch(setProductList({ ...product, quantity: updatedQuantity }));
+
+      // Return the updated quantity to update the state
+      return updatedQuantity;
+    });
   };
 
   return (
@@ -27,23 +28,26 @@ const Product = ({ product }) => {
       className="bg-[cadetblue] flex h-72 w-72 flex-wrap border-black border-2 m-5 p-5 jusify-center items-center rounded-lg text-center"
     >
       <span className="text-2xl text-center w-full">{product?.title}</span>
-      <div className="text-xl w-full">{`Price: ${product?.price}`}</div>
+      <div className="text-xl w-full">{`Price: ${round(
+        product?.price,
+        2
+      )}`}</div>
       <div className="text-xl w-full">{`Quantity: ${quantity}`}</div>
-      <div className="w-full">
+      <div className="w-full flex items-center justify-center">
         <button
-          className="bg-[bisque] text-4xl border-2 border-black rounded-full"
+          className="bg-amber-600 text-4xl border-2 border-black rounded-full"
           onClick={() => quantity >= 1 && setQuantity(quantity - 1)}
         >
           -
         </button>
         <span
-          className="text-xl border-2 border-black mx-3"
+          className="text-xl border-2 border-black mx-3 p-1 cursor-pointer rounded-md"
           onClick={() => handleAddToCart(product)}
         >
           Add to Cart
         </span>
         <button
-          className="bg-[bisque] text-4xl border-2 border-black rounded-full"
+          className="bg-amber-600 text-4xl border-2 border-black rounded-full"
           onClick={() => setQuantity(quantity + 1)}
         >
           +
---
+++ src/components/ProductList.jsx
@@ -0,0 +1,34 @@
+import { useDispatch, useSelector } from "react-redux";
+import Product from "./Product";
+import { CircularProgress } from "@mui/material";
+import { useEffect } from "react";
+import { getAllProductsAction } from "../redux/reducers/products";
+import { isEmpty } from "lodash";
+
+const ProductList = () => {
+  const dispatch = useDispatch();
+  const { products } = useSelector((state) => state.products);
+
+  useEffect(() => {
+    dispatch(getAllProductsAction());
+  }, []);
+  return (
+    <div className="flex w-screen flex-wrap justify-center mt-[-10px]">
+      {products?.isProductsLoading ? (
+        <div className="h-screen flex items-center">
+          <CircularProgress size={50} />
+        </div>
+      ) : isEmpty(products?.productsData) ? (
+        <div className="h-screen flex items-center">
+          <span className="text-3xl">No Product Available</span>
+        </div>
+      ) : (
+        products?.productsData?.map((product) => {
+          return <Product key={product?.id} product={product} />;
+        })
+      )}
+    </div>
+  );
+};
+
+export default ProductList;
